20e3457bfd9126efb9d0be37f4003e8e858c975e
antonio.hueso <antonio.hueso@gmail.com>
2022-04-02 08:36:44 +0200

    pruebas

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Task struct {
 	Name string
 	Age  int
@@ -7,31 +9,83 @@ type Task struct {
 
 type Project struct {
 	TotalTask int
-	Tasks     []*Task
+	Tasks     []Task
+}
+
+type ImmutableSlice[K any] interface {
+	Add(v K) ImmutableSlice[K]
+	Get(index int) K
+	Set(v K, index int) ImmutableSlice[K]
+	Slice() []K
+}
+
+type immutableSlice[K any] struct {
+	slice []K
+}
+
+func (s immutableSlice[K]) Add(v K) ImmutableSlice[K] {
+	var newSlice = copySlice(s.slice)
+	s.slice = newSlice
+	s.slice = append(s.slice, v)
+	return s
+}
+
+func (s immutableSlice[K]) Set(v K, index int) ImmutableSlice[K] {
+	var newSlice = copySlice(s.slice)
+	s.slice = newSlice
+	s.slice[index] = v
+	return s
+}
+
+func (s immutableSlice[K]) Get(index int) K {
+	return s.slice[index]
+}
+
+func (s immutableSlice[K]) Slice() []K {
+	return copySlice(s.slice)
+}
+
+func copySlice[K any](slice []K) []K {
+	var newSlice []K
+	newSlice = append(newSlice, slice...)
+	return newSlice
+}
+
+func NewImmutableSlice[K any](slice ...K) ImmutableSlice[K] {
+	return immutableSlice[K]{
+		slice: slice,
+	}
 }
 
 func main() {
 
-	var project = &Project{
-		TotalTask: 10,
-		Tasks: []*Task{
-			&Task{Name: "Antoño", Age: 10},
-			&Task{Name: "Antoño1", Age: 11},
-			&Task{Name: "Antoño2", Age: 12},
-		},
+	var p = NewImmutableSlice[Task]()
+
+	for i := 0; i < 10; i++ {
+		p = p.Add(Task{Name: fmt.Sprintf("Pollas %d", i), Age: 10 + i})
 	}
 
-	var p = *project
-	var tasks []*Task
-	for _, t := range p.Tasks {
-		var tt = *t
-		tasks = append(tasks, &tt)
-	}
-	p.Tasks = tasks
+	var s = p.Get(0)
+	s.Name = "Pene"
+	p.Set(s, 0)
 
-	p.TotalTask = 2
+	for _, s := range p.Slice() {
+		fmt.Println(s.Name, s.Age)
+	}
+
+}
+
+func prueba(p Project) Project {
+	p.TotalTask = 10
+	p.Tasks = []Task{
+		{Name: "Antoño", Age: 10},
+		{Name: "Antoño1", Age: 11},
+		{Name: "Antoño2", Age: 12},
+	}
+	return p
+}
+
+func prueba2(p Project) Project {
 	p.Tasks[0].Name = "Pollas"
-
-	println(p.TotalTask, " ", project.TotalTask)
-	println(p.Tasks[0].Name, " ", project.Tasks[0].Name)
+	return p
 }
